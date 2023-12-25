dc01a287d2fec6b2a1efa805acf2fc7228725e83
Gaurav Kanani <rw4.gaurav.bk@gmail.com>
2023-12-25 12:49:04 +0530

    Carousel Slider

--- lib/modules/utils/tabs/gallery/gallery.dart
+++ lib/modules/utils/tabs/gallery/gallery.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class GalleryTab extends StatelessWidget {
   const GalleryTab({super.key});
@@ -6,9 +7,29 @@ class GalleryTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Gallery Tab"),
+        body: CarouselSlider(
+      items: Colors.primaries
+          .map((e) => Container(
+                color: e,
+                alignment: Alignment.center,
+                child: const Text("COLOR"),
+              ))
+          .toList(),
+      options: CarouselOptions(
+        height: 400,
+        // aspectRatio: 16 / 9,
+        // viewportFraction: 0.8,
+        enableInfiniteScroll: true,
+        autoPlay: true,
+        initialPage: 0,
+        autoPlayAnimationDuration: const Duration(seconds: 1),
+        autoPlayInterval: const Duration(seconds: 1),
+        autoPlayCurve: Curves.linear,
+        enlargeCenterPage: true,
+        enlargeFactor: 0.5,
+        scrollDirection: Axis.horizontal,
+        onPageChanged: (i, _) {},
       ),
-    );
+    ));
   }
 }
